2d0ef2584d495de9570264a03f27accca86ad6f7
Jhan Ramirez <jhannavarro2001@gmail.com>
2022-09-12 16:17:46 -0500

    add

--- routes/tecnico.js
+++ routes/tecnico.js
@@ -3,146 +3,180 @@ const router = express.Router();
 
 const tecnicoController = require("../controllers/tecnicoController");
 
+// Dashboard
 router.get("/", tecnicoController.isAuthenticatedTecnic, (req, res) => {
   res.render("tecnico/main", { user: req.user });
 });
 
-// Apartado: Consolidaciones
+// Apartado: Visitas
 router.get(
-  "/Consolidaciones/ESPublica",
+  "/Consolidaciones/",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/ESPublica", {
+    res.render("tecnico/Visitas/mainVisit", {
+      user: req.user,
+    });
+  }
+);
+
+// Visitas - Ver
+router.get(
+  "/Consolidaciones/Ver",
+  tecnicoController.isAuthenticatedTecnic,
+  (req, res) => {
+    res.render("tecnico/Visitas/Ver/mainVer", {
+      user: req.user,
+    });
+  }
+);
+
+// Visitas - Enviar
+
+router.get(
+  "/Consolidaciones/Enviar",
+  tecnicoController.isAuthenticatedTecnic,
+  (req, res) => {
+    res.render("tecnico/Visitas/Enviar/mainEnviar", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/Establecimientos",
+  "/Consolidaciones/Enviar/ESPublica",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/Establecimientos", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/ESPublica", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/Vehiculos",
+  "/Consolidaciones/Enviar/Establecimientos",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/Vehiculos", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/Establecimientos", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/Cementerios",
+  "/Consolidaciones/Enviar/Vehiculos",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/Cementerios", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/Vehiculos", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/Morgues",
+  "/Consolidaciones/Enviar/Cementerios",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/Morgues", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/Cementerios", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/MSEstablecimientos",
+  "/Consolidaciones/Enviar/Morgues",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/MSEstablecimientos", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/Morgues", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/MSProductos",
+  "/Consolidaciones/Enviar/MSEstablecimientos",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/MSProductos", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/MSEstablecimientos", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/Quejas",
+  "/Consolidaciones/Enviar/MSProductos",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/Quejas", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/MSProductos", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/TomaMuestras",
+  "/Consolidaciones/Enviar/Quejas",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/TomaMuestras", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/Quejas", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/ViviendaSaludable",
+  "/Consolidaciones/Enviar/TomaMuestras",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/ViviendaSaludable", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/TomaMuestras", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/CarnetsBPM",
+  "/Consolidaciones/Enviar/ViviendaSaludable",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/CarnetsBPM", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/ViviendaSaludable", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/IVCRotulado",
+  "/Consolidaciones/Enviar/CarnetsBPM",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/IVCRotulado", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/CarnetsBPM", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/IVCPublicidad",
+  "/Consolidaciones/Enviar/IVCRotulado",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/IVCPublicidad", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/IVCRotulado", {
       user: req.user,
     });
   }
 );
 
 router.get(
-  "/Consolidaciones/EduSanitaria",
+  "/Consolidaciones/Enviar/IVCPublicidad",
   tecnicoController.isAuthenticatedTecnic,
   (req, res) => {
-    res.render("tecnico/Consolidaciones/EduSanitaria", {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/IVCPublicidad", {
+      user: req.user,
+    });
+  }
+);
+
+router.get(
+  "/Consolidaciones/Enviar/EduSanitaria",
+  tecnicoController.isAuthenticatedTecnic,
+  (req, res) => {
+    res.render("tecnico/Visitas/Enviar/Consolidaciones/EduSanitaria", {
       user: req.user,
     });
   }
