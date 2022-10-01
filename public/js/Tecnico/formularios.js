e65f7ac8e3968e27ceac46316b386ff86c9dafcd
desksoul29 <jhannavarro2001@gmail.com>
2022-10-01 01:07:16 -0500

    Arreglos de quejas, establecimientos

--- public/js/Tecnico/formularios.js
+++ public/js/Tecnico/formularios.js
@@ -33,6 +33,81 @@ var toast = (function () {
   };
 })();
 
+var time = (function () {
+  var formatDate = function (date) {
+    return (
+      date.getFullYear() +
+      "-" +
+      time.pad(date.getMonth() + 1) +
+      "-" +
+      time.pad(date.getDate())
+    );
+  };
+
+  var min = function (date) {
+    if (date.getDate() <= 5) {
+      return date.getFullYear() + "-" + time.pad(date.getMonth()) + "-06";
+    } else {
+      return date.getFullYear() + "-" + time.pad(date.getMonth() + 1) + "-06";
+    }
+  };
+
+  var max = function (date) {
+    if (date.getDate() <= 5) {
+      return date.getFullYear() + "-" + time.pad(date.getMonth() + 1) + "-05";
+    } else {
+      if (date.getMonth() + 2 != 13) {
+        return date.getFullYear() + "-" + time.pad(date.getMonth() + 2) + "-05";
+      } else {
+        return date.getFullYear() + 1 + "-01-05";
+      }
+    }
+  };
+
+  var pad = function (number) {
+    if (number < 10) {
+      return "0" + number;
+    }
+    return number;
+  };
+
+  return {
+    formatDate: formatDate,
+    pad: pad,
+    min: min,
+    max: max,
+  };
+})();
+
+let MESES = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+const date = new Date();
+
+if (document.getElementById("mes") != null) {
+  if (date.getDate() < 5) {
+    document.getElementById("mes").value = MESES[date.getMonth() - 1];
+  } else {
+    document.getElementById("mes").value = MESES[date.getMonth()];
+  }
+}
+
+document.getElementById("fVisit").setAttribute("min", time.min(date));
+document.getElementById("fVisit").setAttribute("max", time.max(date));
+document.getElementById("fVisit").value = time.formatDate(date);
+
 function eventSalud(e) {
   if (
     document.getElementById("muniSelect").value == "Seleccione el Municipio" ||
@@ -110,7 +185,7 @@ function eduSanitaria(e) {
     document.getElementById("intensidadCap").value == "" ||
     document.getElementById("totalPersCap").value == "" ||
     document.getElementById("lugCap").value == 0 ||
-    document.getElementById("personalCap").value == 0
+    document.getElementById("personalCap").value == "Seleccione"
   ) {
     toast.toastInfo("Advertencia", "Ingresar todos los Campos");
     e.preventDefault();
@@ -170,21 +245,64 @@ function establecimientosForm(e) {
     e.preventDefault();
   }
 
-  if (document.getElementById("IVC").value == "ROTULADO") {
-    if (document.getElementById("productoRotulado").value == 0) {
-      toast.toastInfo("Advertencia", "Ingresar todos los Campos");
+  if (document.getElementById("publicidadON").checked) {
+    //Publicidad
+    if (
+      document.getElementById("permisoSanitario").value == 0 ||
+      document.getElementById("productoPublicidad").value == 0 ||
+      document.getElementById("marcaPublicidad").value == 0 ||
+      document.getElementById("medPubli").value == "Seleccione"
+    ) {
+      toast.toastInfo(
+        "Advertencia",
+        "Ingresar todos los Campos de IVC Publicidad"
+      );
       e.preventDefault();
     }
   }
 
-  if (document.getElementById("IVC").value == "PUBLICIDAD") {
+  if (document.getElementById("rotuladoON").checked) {
+    //Rotulado
+    if (document.getElementById("productoRotulado").value == 0) {
+      toast.toastInfo(
+        "Advertencia",
+        "Ingresar todos los Campos de IVC Rotulado"
+      );
+      e.preventDefault();
+    }
+  }
+
+  if (document.getElementById("establecimientosON").checked) {
+    //Medidas Sanitarias a Establecimientos
     if (
-      document.getElementById("permisoSanitario").value == 0 ||
-      document.getElementById("productoPublicidad").value == 0 ||
-      document.getElementById("marca").value == 0 ||
-      document.getElementById("medPubli").value == "Seleccione"
+      document.getElementById("motivoApli").value == 0 ||
+      document.getElementById("medidaApliEstable").value == "Seleccione"
     ) {
-      toast.toastInfo("Advertencia", "Ingresar todos los Campos");
+      toast.toastInfo(
+        "Advertencia",
+        "Ingresar todos los Campos de Medidas Sanitarias a Establecimientos"
+      );
+      e.preventDefault();
+    }
+  }
+
+  if (document.getElementById("productosON").checked) {
+    //Medidas Sanitarias a Productos
+    if (
+      document.getElementById("medidaApliProduc").value == "Seleccione" ||
+      document.getElementById("permisoProduco").value == 0 ||
+      document.getElementById("productoMed").value == 0 ||
+      document.getElementById("marcaProduct").value == 0 ||
+      document.getElementById("motivoProduct").value == 0 ||
+      document.getElementById("presentProduct").value == 0 ||
+      document.getElementById("cantProdu").value == 0 ||
+      document.getElementById("fabriProduc").value == 0 ||
+      document.getElementById("loteProduc").value == 0
+    ) {
+      toast.toastInfo(
+        "Advertencia",
+        "Ingresar todos los Campos de Medidas Sanitarias a Productos"
+      );
       e.preventDefault();
     }
   }
@@ -228,24 +346,5 @@ function cementerioForm(e) {
   ) {
     toast.toastInfo("Advertencia", "Ingresar todos los Campos");
     e.preventDefault();
-  }
-}
-
-function medSaniEstaForm(e) {
-  if (
-    document.getElementById("rSocial").value ==
-      "Seleccione El Establecimiento" ||
-    document.getElementById("solicitudApli").value == "Seleccione" ||
-    document.getElementById("motivoApli").value == 0 ||
-    document.getElementById("fApli").value == 0 ||
-    document.getElementById("medidaApli").value == "Seleccione"
-  ) {
-    toast.toastInfo("Advertencia", "Ingresar todos los Campos");
-    e.preventDefault();
-  } else if (document.getElementById("solicitudApli").value == "SI") {
-    if (document.getElementById("fechSancio").value == 0) {
-      toast.toastInfo("Advertencia", "Ingresar todos los Campos");
-      e.preventDefault();
-    }
   }
 }
